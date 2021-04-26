5cc0fb589e5dc4a1cef0e5f7a92da9b1b30ee33f
Apun <apun1031@gmail.com>
2021-04-26 14:19:13 +0800

    modify app sql

--- app.py
+++ app.py
@@ -30,24 +30,24 @@ def thankyou():
 def apiattract():
 	page=request.args.get("page","")
 	keyword=request.args.get("keyword","")
+	x=int(page)*12
+	limitnum= str(x)
 	with mydb.cursor() as cursor:
 		if keyword == None or keyword == "" and page != "":
-			sql="SELECT id,stitle,CAT2,xbody,address,info,MRT,latitude,longitude,file FROM travel"
+			sqlcount="SELECT count(*) FROM travel"
+			cursor.execute(sqlcount)
+			resultCount = cursor.fetchall()
+			for a in resultCount:
+				allcount = a[0]  # 資料總數
+			num=allcount%12  #餘數
+			newnum=int(allcount/12)  #整數
+			sql="SELECT id,stitle,CAT2,xbody,address,info,MRT,latitude,longitude,file FROM website.travel LIMIT "+limitnum+",12 "
 			cursor.execute(sql)
 			myresult = cursor.fetchall() 
+			travellist=[]
 			count=len(myresult)
-			if  count > 12 and count != 0:
-				num=count%12  #餘數
-				newnum=int(count/12)  #整數
-				if num != 0:
-						if int(page) < newnum +1 :
-							travellist=[]
-							a=int(page)*12
-							if int(page) == newnum:
-								b=count
-							else:
-								b=a+12
-							for i in range(a,b):
+			if count != 0:
+							for i in range(0,count):
 								data=myresult[i]
 								newimg=""
 								img=data[9].split('http')
@@ -66,116 +66,78 @@ def apiattract():
 								newitem['longtitude'] = data[8]
 								newitem['images'] = [newimgQ]
 								travellist.append(newitem)
-							if b==count:
+							if int(page) <= newnum :
+								if num == 0: #沒有餘數:下一頁
+									All={'nextpage':None,'data':travellist} 
+									return jsonify(All)
+								else:
+									if int(page) == newnum:
+										All={'nextpage':None,'data':travellist} 
+										return jsonify(All)
+									else:
+										All={'nextpage':int(page)+1,'data':travellist}
+										return jsonify(All)
+										
+							elif int(page) > newnum:
 								All={'nextpage':None,'data':travellist} 
-							else:
-								All={'nextpage':int(page)+1,'data':travellist}
-							return jsonify(All)
-						else:
-								msg = {"error": True, "message": "查無資料"}
-								return jsonify(msg)
-
-			elif  count<= 12 and count != 0:
-				travellist=[]
-				for row in myresult:
-					newimg=""
-					img=row[9].split('http')
-					for j in img:
-					  newimg += "http"+j+","	
-					  newimgQ=	newimg[:-1]		
-					newitem={}
-					newitem['id'] = int(row[0]) 
-					newitem['name'] = row[1]	
-					newitem['category'] = row[2]
-					newitem['description'] = row[3]
-					newitem['address'] = row[4] 
-					newitem['transport'] = row[5]
-					newitem['mrt'] = row[6]
-					newitem['latitude'] = row[7]
-					newitem['longtitude'] = row[8]
-					newitem['images'] = [newimgQ]
-					travellist.append(newitem)
-				All={'nextpage':None,'data':travellist}  
-				return jsonify(All)
-			elif len(myresult) == 0:
+								return jsonify(All)
+							
+			else:
 				msg = {"error": True, "message": "查無資料"}
 				return jsonify(msg)
 
 		elif keyword != None or keyword != "" and page != "":
-			sql="SELECT id,stitle,CAT2,xbody,address,info,MRT,latitude,longitude,file FROM travel WHERE stitle LIKE '%"+keyword+"%' "
+			sqlcount="SELECT count(*) FROM travel WHERE stitle LIKE '%"+keyword+"%' "
+			cursor.execute(sqlcount)
+			resultCount = cursor.fetchall()
+			for a in resultCount:
+				allcount = a[0]  # 資料總數
+			num=allcount%12  #餘數
+			newnum=int(allcount/12)  #整數
+			sql="SELECT id,stitle,CAT2,xbody,address,info,MRT,latitude,longitude,file FROM travel WHERE stitle LIKE '%"+keyword+"%' LIMIT "+limitnum+",12  "
 			cursor.execute(sql)
 			myresult = cursor.fetchall()  
-			if len(myresult) <= 12 and len(myresult) != 0:
-				if int(page) > 0 :
-					msg = {"error": True, "message": "查無資料"}
-					return jsonify(msg)
-				else:
-					travellist=[]
-				for row in myresult:
-					newimg=""
-					img=row[9].split('http')
-					for j in img:
-					  newimg += "http"+j+","	
-					  newimgQ=	newimg[:-1]		
-					newitem={}
-					newitem['id'] = int(row[0])
-					newitem['name'] = row[1]	
-					newitem['category'] = row[2]
-					newitem['description'] = row[3]
-					newitem['address'] = row[4] 
-					newitem['transport'] = row[5]
-					newitem['mrt'] = row[6]
-					newitem['latitude'] = row[7]
-					newitem['longtitude'] = row[8]
-					newitem['images'] = [newimgQ]
-					travellist.append(newitem)
-				All={'nextpage':None,'data':travellist}  
-				return jsonify(All)
-			elif len(myresult) == 0:
+			travellist=[]
+			count=len(myresult)
+			if count != 0:
+							for i in range(0,count):
+								data=myresult[i]
+								newimg=""
+								img=data[9].split('http')
+								for j in img:
+									newimg += "http"+j+","	
+									newimgQ=newimg[:-1]		
+								newitem={}
+								newitem['id'] = int(data[0])
+								newitem['name'] = data[1]	
+								newitem['category'] = data[2]
+								newitem['description'] = data[3]
+								newitem['address'] = data[4] 
+								newitem['transport'] = data[5]
+								newitem['mrt'] = data[6]
+								newitem['latitude'] = data[7]
+								newitem['longtitude'] = data[8]
+								newitem['images'] = [newimgQ]
+								travellist.append(newitem)
+							if int(page) <= newnum :
+								if num == 0: #沒有下一頁
+									All={'nextpage':None,'data':travellist} 
+									return jsonify(All)
+								else:
+									if int(page) == newnum:
+										All={'nextpage':None,'data':travellist} 
+										return jsonify(All)
+									else:
+										All={'nextpage':int(page)+1,'data':travellist}
+										return jsonify(All)
+										
+							elif int(page) > newnum:
+								All={'nextpage':None,'data':travellist} 
+								return jsonify(All)
+							
+			else:
 				msg = {"error": True, "message": "查無資料"}
 				return jsonify(msg)
-			elif len(myresult) > 12:
-				num=len(myresult)%12  #餘數
-				newnum= int(len(myresult)/12) #整數
-				rangeone=0
-				rangetwo=0
-				if num != 0:
-						if int(page) < newnum +1 :
-							travellist=[]
-							rangeone=int(page)*12
-							if int(page) == newnum:
-								rangetwo=len(myresult)
-							else:
-								rangetwo=rangeone+12
-						for i in range(rangeone,rangetwo):
-									print(rangeone,rangetwo)
-									data=myresult[i]
-									newimg=""
-									img=data[9].split('http')
-									for j in img:
-										newimg += "http"+j+","	
-										newimgQ=newimg[:-1]		
-									newitem={}
-									newitem['id'] = int(data[0])
-									newitem['name'] = data[1]	
-									newitem['category'] = data[2]
-									newitem['description'] = data[3]
-									newitem['address'] = data[4] 
-									newitem['transport'] = data[5]
-									newitem['mrt'] = data[6]
-									newitem['latitude'] = data[7]
-									newitem['longtitude'] = data[8]
-									newitem['images'] = [newimgQ]
-									travellist.append(newitem)
-						if rangetwo==len(myresult):
-							All={'nextpage':None,'data':travellist} 
-							return jsonify(All)
-						elif rangetwo==rangeone+12:
-							All={'nextpage':int(page)+1,'data':travellist} 
-							return jsonify(All)
-						else:
-							msg = {"error": True, "message": "查無資料"}
-							return jsonify(msg)
 
 
 @app.route("/api/attraction/<attractionId>",methods=["GET"])
